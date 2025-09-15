11ece28cc19260f10c42ea09b284272b66e40813
Valeria <76661272+itsval16@users.noreply.github.com>
2025-09-15 11:00:49 -0400

    Add files via upload

--- Week 3/Week 3/Week 3/Week 3/main.cpp
+++ Week 3/Week 3/Week 3/Week 3/main.cpp
@@ -1,42 +1,80 @@
-#include <iostream> // standard libary
+#include <iostream>
+#include <string>
+#include <cctype> // to lower and to upper
+
 
 using namespace std;
 
-template <typename T>
-T maxNum (T a, T b){
-    return (a > b) ? a : b ; //a +b-c;
+bool caseInsensititveCompare( const string& strl, const string& str2){
+    it(str1.length() != str2.length()){
+        return false;
+    }
+    for (size_t i = 0; i < strl.length(); i++){
+        if(tolower(str1[i])) != tolower(str2[i])){
+            return false;
+        }
+    }
+    return true;
+    
 }
 
-//example 1
-//int main() {
-   // int intMax = maxNum(5, 10);
-    //double doubleMax = maxNum(2.5, 11.6);
-//}
-
-//example 2
-//int main () {
-    //int intCalculations = mathFunctions(20,30, 40);
-   // double doubleCalculations = mathFunction(11.11, 22.22, 33.00);
-    
-   // cout << "integer = " << intCalculations << endl;
-   // count << "double = " << doubleCalculations ,, endl;
-    
-   // return 0;
-//}
-
-//example 3: finding the maximum of two values
-//int main(){
-    //int num1= 5, num2= 10;
-   // count << "Max of" << num1 << "and" << num2
-//example 4: vector
 int main(){
-    vector <double> prices = {1,2,3,4,5};
-    prices.push_back(10);
-    prices.push_back(50);
-    prices.push_back(11);
     
-    double total = prices[0] + prices [1] + prices[2];
-    cout << "Total = $" << total << endl;
-    //total 6
+    string str1 = "Hello";
+    string str2 = "Hello";
+    
+    if caseInsensititveCompare(Str1, str2)){
+        cout << "Strings are case-insenstive equal" ,, endl;
+    }
+    }
+
+//    string firstname;
+//    string lastname;
+//    
+//    
+//    cout << "First Name: " << endl;
+//    cin >> firstname;
+//    
+//    cout << "Last Name: " << endl;
+//    cin >> lastname;
+//    
+//    string fullname = firstname + " " + lastname; //
+//    
+//    cout << "Full Name: " << fullname << endl;
+//    
+//    fullname += " Jr.";
+//    cout << " Full Name with suffix: " << fullname << endl;
+//    
+//    fullname = "Dr. " + fullname;
+    
+//    fullname.insert(0, "Dr. ");
+//    cout << "Fullname with Prefix"
+    
+//    string message = "Hello World";
+//    int messagelength = message.length();
+//    int messagesize = message.size();
+//    
+//    cout << "length: " << messagelength << endl << "Size: " << messagesize << endl;
+//    
+//    char firstChar = message[0];
+//    char lastChar = message [message.length()-1];
+//    
+//    cout << "First Char: " <<firstChar << endl << "Last Char: " << lastChar << endl;
+    
+//    string str1 = "apple";
+//    string str2 = " Banana";
+//    
+//    if (str1 == str2){
+//        cout << "Both Strings are equal!" << endl;
+//    } else if (str1 > str2){
+//        cout << " Strl is lexicographically greater than str2." << endl;
+//    } else {
+//        cout << "Strl is lexicographically less than str2." << endl;
+//    }
+//    
+    
+    
+    return 0;
 }
 
+// cntl + / = for making a line of code into a comment
